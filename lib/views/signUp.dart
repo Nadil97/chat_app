c99013fababd1a70ee1ee8eac163c15e4b5c3b5f
nadilsandaruwan97@gmail.com <87482209+Nadil97@users.noreply.github.com>
2023-03-22 11:06:15 +0530

    finish  signup ui

--- lib/views/signUp.dart
+++ lib/views/signUp.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/widget/widget.dart';
 import 'package:flutter/material.dart';
 
 class SignUp extends StatefulWidget {
@@ -10,6 +11,99 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: appBarMain(),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height - 40,
+          alignment: Alignment.bottomCenter,
+          child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                      style: simpleTextStyle(),
+                      decoration: textFieldInputDecoration("email")),
+                  TextField(
+                      style: simpleTextStyle(),
+                      decoration: textFieldInputDecoration("password")),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                      child: Text(
+                        'Forget Password !',
+                        style: simpleTextStyle(),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [
+                          const Color(0xff00008B),
+                          const Color(0xff2A75BC)
+                        ]),
+                        borderRadius: BorderRadius.circular(30)),
+                    child: Text(
+                      "Sign up",
+                      style: simpleTextStyle(),
+                    ),
+                  ),
+                  // signIn with Google button
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30)),
+                    child: Text(
+                      "Sign up with Google",
+                      style: TextStyle(color: Colors.black87, fontSize: 17),
+                    ),
+                  ),
+                  //
+                  SizedBox(
+                    height: 10,
+                  ),
+                  // cretae register now option
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Dont have account ? ',
+                        style: mediamTextStyle(),
+                      ),
+                      Text(
+                        'Register Now',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 17,
+                            decoration: TextDecoration.underline),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 70,
+                  )
+                ],
+              )),
+        ),
+      ),
+    );
   }
 }
