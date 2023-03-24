3123576605775e5fe74fa0db14db500fd9eda0f4
nadilsandaruwan97@gmail.com <87482209+Nadil97@users.noreply.github.com>
2023-03-24 13:20:00 +0530

    save the user name and e mail database

--- lib/service/database.dart
+++ lib/service/database.dart
@@ -1,5 +1,8 @@
-class DatabaseMethods{
-  getUserByUsername(String username){
-    
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseMethods {
+  getUserByUsername(String username) {}
+  uploadUserInfo(userMap) {
+    FirebaseFirestore.instance.collection('users').add(userMap);
   }
 }
--- lib/views/signUp.dart
+++ lib/views/signUp.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/service/auth.dart';
+import 'package:chat_app/service/database.dart';
 import 'package:chat_app/widget/widget.dart';
 import 'package:flutter/material.dart';
 
@@ -16,6 +17,7 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   bool isLoading = false;
   AuthMethod authMethods = new AuthMethod();
+  DatabaseMethods databaseMethods = new DatabaseMethods();
   final formKey = GlobalKey<FormState>();
   TextEditingController _userNameController = new TextEditingController();
   TextEditingController _emailController = new TextEditingController();
@@ -31,6 +33,12 @@ class _SignUpState extends State<SignUp> {
               _emailController.text, _passwordController.text)
           .then((val) {
         // print("${val.uId}");
+
+        Map<String, String> userInfoMap = {
+          "name": _userNameController.text,
+          "email": _emailController.text
+        };
+        databaseMethods.uploadUserInfo(userInfoMap);
         Navigator.pushReplacement(
             context, MaterialPageRoute(builder: (context) => ChatRoom()));
       });
